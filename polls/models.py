908ad286f01f4a06afafb9565af3ce74dad6b6b5
james31366 <james31366@gmail.com>
2020-09-20 01:43:22 +0700

    Send Iteration 2

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('end date')
 
     def __str__(self):
         return self.question_text
@@ -15,9 +16,21 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def is_published(self):
+        now = timezone.now()
+        return self.pub_date <= now
+
+    def can_vote(self):
+        now = timezone.now()
+        return self.pub_date <= now <= self.end_date
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+
+    is_published.short_description = 'Published?'
+    is_published.boolean = True
+    is_published.admin_order_field = 'pub_date'
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -13,10 +14,10 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
+        Return all published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
@@ -37,16 +38,20 @@ class ResultsView(generic.DetailView):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        select_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form
-        return render(request, 'polls/detail.html',
-                      {'question': question, 'error_message': "You didn't select a choice.", })
+    if question.can_vote():
+        try:
+            select_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            # Redisplay the question voting form
+            return render(request, 'polls/detail.html',
+                          {'question': question, 'error_message': "You didn't select a choice.", })
+        else:
+            select_choice.votes += 1
+            select_choice.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     else:
-        select_choice.votes += 1
-        select_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        messages.error(request, "Polls not published yet or does not exist")
+        return redirect('polls:index')
